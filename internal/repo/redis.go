2ff4ea81325de46a3ffb35741a16a980f3cb4402
aog-developer <elisandil88@gmail.com>
2025-11-21 09:00:19 +0100

    feat: Add redis repository implementation

---
+++ internal/repo/redis.go
@@ -0,0 +1,66 @@
+package repo
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"time"
+
+	"github.com/Elisandil/GoSnap/internal/domain"
+	"github.com/redis/go-redis/v9"
+)
+
+type RedisRepo struct {
+	client *redis.Client
+	ttl    time.Duration
+}
+
+func NewRedisRepo(client *redis.Client, ttl time.Duration) *RedisRepo {
+	return &RedisRepo{
+		client: client,
+		ttl:    ttl,
+	}
+}
+
+// Set stores in cache the URL associated with the given short code in Redis with a TTL.
+func (r *RedisRepo) Set(ctx context.Context, shortCode string, url *domain.URL) error {
+	data, err := json.Marshal(url)
+	if err != nil {
+		return err
+	}
+
+	return r.client.Set(ctx, shortCode, data, r.ttl).Err()
+}
+
+// Get retrieves from cache the URL associated with the given short code in Redis.
+func (r *RedisRepo) Get(ctx context.Context, shortCode string) (*domain.URL, error) {
+	data, err := r.client.Get(ctx, shortCode).Bytes()
+	if err != nil {
+		if errors.Is(err, redis.Nil) {
+			return nil, ErrNotFound
+		}
+		return nil, err
+	}
+
+	var url domain.URL
+	if err := json.Unmarshal(data, &url); err != nil {
+		return nil, err
+	}
+
+	return &url, nil
+}
+
+// Delete removes from cache the URL associated with the given short code in Redis.
+func (r *RedisRepo) Delete(ctx context.Context, shortCode string) error {
+	return r.client.Del(ctx, shortCode).Err()
+}
+
+// Exists checks if a short code exists in Redis.
+func (r *RedisRepo) Exists(ctx context.Context, shortCode string) (bool, error) {
+	result, err := r.client.Exists(ctx, shortCode).Result()
+	if err != nil {
+		return false, err
+	}
+
+	return result != 0, nil
+}
