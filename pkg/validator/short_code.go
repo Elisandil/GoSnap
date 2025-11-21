7cbd7ab14a67d222040ff347119229e1b4d1b3f3
aog-developer <elisandil88@gmail.com>
2025-11-21 16:18:22 +0100

    feat: Add short code validator file

---
+++ pkg/validator/short_code.go
@@ -0,0 +1,29 @@
+package validator
+
+import (
+	"regexp"
+)
+
+const (
+	MinShortCodeLength = 1
+	MaxShortCodeLength = 10
+)
+
+var validShortCodeRegex = regexp.MustCompile(`^[0-9A-Za-z]+$`)
+
+// IsValidShortCode checks if the provided short code is valid.
+// A valid short code is between MinShortCodeLength and MaxShortCodeLength characters
+// and contains only alphanumeric characters.
+func IsValidShortCode(shortCode string) bool {
+
+	if shortCode == "" {
+		return false
+	}
+
+	length := len(shortCode)
+	if length < MinShortCodeLength || length > MaxShortCodeLength {
+		return false
+	}
+
+	return validShortCodeRegex.MatchString(shortCode)
+}
